da517c825f9d707b92befbaf2766fe02acbc315c
ChpssCode <paaxyman@gmail.com>
2024-01-08 21:29:13 +0000

    change ip

--- client/src/scripts/config.ts
+++ client/src/scripts/config.ts
@@ -1,7 +1,7 @@
 export const Config = {
     regions: {
-        onevone: { name: "1v1 Server", address: "104.207.143.117", https: false },
-        clanwar: { name: "Clanwar Server", address: "104.207.143.117:8001", https: false },
+        onevone: { name: "1v1 Server", address: "82.165.212.40", https: false },
+        clanwar: { name: "Clanwar Server", address: "82.165.212.40:8001", https: false },
         na: { name: "North America", address: "na.suroi.io", https: true },
         eu: { name: "Europe", address: "eu.suroi.io", https: true },
         sa: { name: "South America", address: "sa.suroi.io", https: true },
